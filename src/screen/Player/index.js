5c5f45d7e493feb2eeb8a4c83077145a1bbd9c14
giampaolli <giampaoli2011@gmail.com>
2019-01-24 07:31:56 -0200

    styling player component

--- src/screen/Player/index.js
+++ src/screen/Player/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {
-    Dimensions,
     Image,
     Slider,
     StyleSheet,
     Text,
-    TouchableHighlight,
+    TouchableOpacity,
     View,
 } from 'react-native';
-import { Asset, Audio, Font, Video } from 'expo';
+import { Asset, Audio, Font } from 'expo';
 import { MaterialIcons } from '@expo/vector-icons';
 
 class Icon {
@@ -20,75 +19,28 @@ class Icon {
     }
 }
 
-class PlaylistItem {
-    constructor(name, uri, isVideo) {
-        this.name = name;
-        this.uri = uri;
-        this.isVideo = isVideo;
-    }
-}
-
-const PLAYLIST = [
-    new PlaylistItem(
-        'Comfort Fit - “Sorry”',
-        'https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Comfort_Fit_-_03_-_Sorry.mp3',
-        false
-    ),
-    new PlaylistItem(
-        'Big Buck Bunny',
-        'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-        true
-    ),
-    new PlaylistItem(
-        'Mildred Bailey – “All Of Me”',
-        'https://ia800304.us.archive.org/34/items/PaulWhitemanwithMildredBailey/PaulWhitemanwithMildredBailey-AllofMe.mp3',
-        false
-    ),
-    new PlaylistItem(
-        "Popeye - I don't scare",
-        'https://ia800501.us.archive.org/11/items/popeye_i_dont_scare/popeye_i_dont_scare_512kb.mp4',
-        true
-    ),
-    new PlaylistItem(
-        'Podington Bear - “Rubber Robot”',
-        'https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Podington_Bear_-_Rubber_Robot.mp3',
-        false
-    ),
-];
-
 const ICON_PLAY_BUTTON = new Icon(require('../../../assets/images/play_button.png'), 34, 51);
 const ICON_PAUSE_BUTTON = new Icon(require('../../../assets/images/pause_button.png'), 34, 51);
 const ICON_STOP_BUTTON = new Icon(require('../../../assets/images/stop_button.png'), 22, 22);
 
 const ICON_MUTED_BUTTON = new Icon(require('../../../assets/images/muted_button.png'), 67, 58);
-const ICON_UNMUTED_BUTTON = new Icon(require('../../../assets/images/unmuted_button.png'), 67, 58);
 
 const ICON_TRACK_1 = new Icon(require('../../../assets/images/track_1.png'), 166, 5);
 const ICON_THUMB_1 = new Icon(require('../../../assets/images/thumb_1.png'), 18, 19);
 const ICON_THUMB_2 = new Icon(require('../../../assets/images/thumb_2.png'), 15, 19);
 
-const LOOPING_TYPE_ALL = 0;
-const LOOPING_TYPE_ONE = 1;
-
-const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = Dimensions.get('window');
 const BACKGROUND_COLOR = '#FFF8ED';
 const DISABLED_OPACITY = 0.5;
-const FONT_SIZE = 14;
-const LOADING_STRING = '... loading ...';
 const BUFFERING_STRING = '...buffering...';
-const VIDEO_CONTAINER_HEIGHT = DEVICE_HEIGHT * 2.0 / 5.0 - FONT_SIZE * 2;
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
-        this.index = 0;
         this.isSeeking = false;
         this.shouldPlayAtEndOfSeek = false;
         this.playbackInstance = null;
         this.state = {
-            showVideo: false,
-            playbackInstanceName: LOADING_STRING,
-            loopingType: LOOPING_TYPE_ALL,
+            loopingType: 0,
             muted: false,
             playbackInstancePosition: null,
             playbackInstanceDuration: null,
@@ -100,12 +52,6 @@ export default class App extends React.Component {
             shouldCorrectPitch: true,
             volume: 1.0,
             rate: 1.0,
-            videoWidth: DEVICE_WIDTH,
-            videoHeight: VIDEO_CONTAINER_HEIGHT,
-            poster: false,
-            useNativeControls: false,
-            fullscreen: false,
-            throughEarpiece: false,
         };
     }
 
@@ -145,22 +91,16 @@ export default class App extends React.Component {
             shouldCorrectPitch: this.state.shouldCorrectPitch,
             volume: this.state.volume,
             isMuted: this.state.muted,
-            isLooping: this.state.loopingType === LOOPING_TYPE_ONE,
+            isLooping: this.state.loopingType === 1,
         };
 
-        if (PLAYLIST[this.index].isVideo) {
-            this._video.setOnPlaybackStatusUpdate(this._onPlaybackStatusUpdate);
-            await this._video.loadAsync(source, initialStatus);
-            this.playbackInstance = this._video;
-            const status = await this._video.getStatusAsync();
-        } else {
-            const { sound, status } = await Audio.Sound.create(
-                source,
-                initialStatus,
-                this._onPlaybackStatusUpdate
-            );
-            this.playbackInstance = sound;
-        }
+        const { sound, status } = await Audio.Sound.create(
+            source,
+            initialStatus,
+            this._onPlaybackStatusUpdate
+        );
+        this.playbackInstance = sound;
+
 
         this._updateScreenForLoading(false);
     }
@@ -168,17 +108,13 @@ export default class App extends React.Component {
     _updateScreenForLoading(isLoading) {
         if (isLoading) {
             this.setState({
-                showVideo: false,
                 isPlaying: false,
-                playbackInstanceName: LOADING_STRING,
                 playbackInstanceDuration: null,
                 playbackInstancePosition: null,
                 isLoading: true,
             });
         } else {
             this.setState({
-                playbackInstanceName: PLAYLIST[this.index].name,
-                showVideo: PLAYLIST[this.index].isVideo,
                 isLoading: false,
             });
         }
@@ -195,11 +131,10 @@ export default class App extends React.Component {
                 rate: status.rate,
                 muted: status.isMuted,
                 volume: status.volume,
-                loopingType: status.isLooping ? LOOPING_TYPE_ONE : LOOPING_TYPE_ALL,
+                loopingType: status.isLooping ? 1 : 0,
                 shouldCorrectPitch: status.shouldCorrectPitch,
             });
             if (status.didJustFinish && !status.isLooping) {
-                this._advanceIndex(true);
                 this._updatePlaybackInstanceForIndex(true);
             }
         } else {
@@ -209,17 +144,8 @@ export default class App extends React.Component {
         }
     };
 
-    _advanceIndex(forward) {
-        this.index = (this.index + (forward ? 1 : PLAYLIST.length - 1)) % PLAYLIST.length;
-    }
-
     async _updatePlaybackInstanceForIndex(playing) {
         this._updateScreenForLoading(true);
-
-        this.setState({
-            videoWidth: DEVICE_WIDTH,
-            videoHeight: VIDEO_CONTAINER_HEIGHT,
-        });
 
         this._loadNewPlaybackInstance(playing);
     }
@@ -237,12 +163,6 @@ export default class App extends React.Component {
     _onStopPressed = () => {
         if (this.playbackInstance != null) {
             this.playbackInstance.stopAsync();
-        }
-    };
-
-    _onMutePressed = () => {
-        if (this.playbackInstance != null) {
-            this.playbackInstance.setIsMutedAsync(!this.state.muted);
         }
     };
 
@@ -315,13 +235,13 @@ export default class App extends React.Component {
         const { navigation } = this.props;
         const feed = navigation.getParam('feed', {});
 
-        return !this.state.fontLoaded ? (
-            <View style={styles.emptyContainer} />
-        ) : (
+        return (
             <View style={styles.container}>
-                <View />
+                <View style={styles.imageContainer}>
+                    <Image source={feed.thumbnail} resizeMode={'center'} />
+                </View>
                 <View style={styles.nameContainer}>
-                    <Text style={[styles.text, { fontFamily: 'cutive-mono-regular' }]}>
+                    <Text style={styles.text}>
                         {feed.title}
                     </Text>
                 </View>
@@ -343,10 +263,10 @@ export default class App extends React.Component {
                         disabled={this.state.isLoading}
                     />
                     <View style={styles.timestampRow}>
-                        <Text style={[styles.text, styles.buffering, { fontFamily: 'cutive-mono-regular' }]}>
+                        <Text style={[styles.text, styles.buffering, ]}>
                             {this.state.isBuffering ? BUFFERING_STRING : ''}
                         </Text>
-                        <Text style={[styles.text, styles.timestamp, { fontFamily: 'cutive-mono-regular' }]}>
+                        <Text style={[styles.text, styles.timestamp, ]}>
                             {this._getTimestamp()}
                         </Text>
                     </View>
@@ -359,7 +279,7 @@ export default class App extends React.Component {
                             opacity: this.state.isLoading ? DISABLED_OPACITY : 1.0,
                         },
                     ]}>
-                    <TouchableHighlight
+                    <TouchableOpacity
                         underlayColor={BACKGROUND_COLOR}
                         style={styles.wrapper}
                         onPress={this._onPlayPausePressed}
@@ -368,26 +288,17 @@ export default class App extends React.Component {
                             style={styles.button}
                             source={this.state.isPlaying ? ICON_PAUSE_BUTTON.module : ICON_PLAY_BUTTON.module}
                         />
-                    </TouchableHighlight>
-                    <TouchableHighlight
+                    </TouchableOpacity>
+                    <TouchableOpacity
                         underlayColor={BACKGROUND_COLOR}
                         style={styles.wrapper}
                         onPress={this._onStopPressed}
                         disabled={this.state.isLoading}>
                         <Image style={styles.button} source={ICON_STOP_BUTTON.module} />
-                    </TouchableHighlight>
+                    </TouchableOpacity>
                 </View>
                 <View style={[styles.buttonsContainerBase, styles.buttonsContainerMiddleRow]}>
                     <View style={styles.volumeContainer}>
-                        <TouchableHighlight
-                            underlayColor={BACKGROUND_COLOR}
-                            style={styles.wrapper}
-                            onPress={this._onMutePressed}>
-                            <Image
-                                style={styles.button}
-                                source={this.state.muted ? ICON_MUTED_BUTTON.module : ICON_UNMUTED_BUTTON.module}
-                            />
-                        </TouchableHighlight>
                         <Slider
                             style={styles.volumeSlider}
                             trackImage={ICON_TRACK_1.module}
@@ -404,107 +315,91 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
     emptyContainer: {
-        // alignSelf: 'stretch',
-        // backgroundColor: BACKGROUND_COLOR,
+        alignSelf: 'stretch',
     },
     container: {
-        // flex: 1,
-        // flexDirection: 'column',
+        flex: 1,
+        flexDirection: 'column',
         // justifyContent: 'space-between',
-        // alignItems: 'center',
-        // alignSelf: 'stretch',
-        // backgroundColor: BACKGROUND_COLOR,
+        alignItems: 'center',
+        alignSelf: 'stretch',
+    },
+    imageContainer: {
+        height: '70%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        height: '80%',
+        width: '60%',
     },
     wrapper: {},
     nameContainer: {
-        // height: FONT_SIZE,
+        height: 14,
     },
     space: {
-        // height: FONT_SIZE,
-    },
-    videoContainer: {
-        // height: VIDEO_CONTAINER_HEIGHT,
-    },
-    video: {
-        // maxWidth: DEVICE_WIDTH,
+        height: 14,
     },
     playbackContainer: {
-        // flex: 1,
-        // flexDirection: 'column',
-        // justifyContent: 'space-between',
-        // alignItems: 'center',
-        // alignSelf: 'stretch',
-        // minHeight: ICON_THUMB_1.height * 2.0,
-        // maxHeight: ICON_THUMB_1.height * 2.0,
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        alignSelf: 'stretch',
+        minHeight: ICON_THUMB_1.height * 2.0,
+        maxHeight: ICON_THUMB_1.height * 2.0,
     },
     playbackSlider: {
-        // alignSelf: 'stretch',
+        alignSelf: 'stretch',
     },
     timestampRow: {
-        // flex: 1,
-        // flexDirection: 'row',
-        // alignItems: 'center',
-        // justifyContent: 'space-between',
-        // alignSelf: 'stretch',
-        // minHeight: FONT_SIZE,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        alignSelf: 'stretch',
+        minHeight: 14,
     },
     text: {
-        // fontSize: FONT_SIZE,
+        fontSize: 14,
         // minHeight: FONT_SIZE,
     },
     buffering: {
-        // textAlign: 'left',
-        // paddingLeft: 20,
+        textAlign: 'left',
+        paddingLeft: 20,
     },
     timestamp: {
-        // textAlign: 'right',
-        // paddingRight: 20,
+        textAlign: 'right',
+        paddingRight: 20,
     },
     button: {
-        // backgroundColor: BACKGROUND_COLOR,
+        backgroundColor: BACKGROUND_COLOR,
     },
     buttonsContainerBase: {
-        // flex: 1,
-        // flexDirection: 'row',
-        // alignItems: 'center',
-        // justifyContent: 'space-between',
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
     },
     buttonsContainerTopRow: {
-        // maxHeight: ICON_PLAY_BUTTON.height,
+        maxHeight: ICON_PLAY_BUTTON.height,
         // minWidth: DEVICE_WIDTH / 2.0,
         // maxWidth: DEVICE_WIDTH / 2.0,
     },
     buttonsContainerMiddleRow: {
-        // maxHeight: ICON_MUTED_BUTTON.height,
-        // alignSelf: 'stretch',
-        // paddingRight: 20,
+        maxHeight: ICON_MUTED_BUTTON.height,
+        alignSelf: 'stretch',
+        paddingRight: 20,
     },
     volumeContainer: {
-        // flex: 1,
-        // flexDirection: 'row',
-        // alignItems: 'center',
-        // justifyContent: 'space-between',
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
         // minWidth: DEVICE_WIDTH / 2.0,
         // maxWidth: DEVICE_WIDTH / 2.0,
     },
     volumeSlider: {
         // width: DEVICE_WIDTH / 2.0 - ICON_MUTED_BUTTON.width,
-    },
-    buttonsContainerBottomRow: {
-        // maxHeight: ICON_THUMB_1.height,
-        // alignSelf: 'stretch',
-        // paddingRight: 20,
-        // paddingLeft: 20,
-    },
-    rateSlider: {
-        // width: DEVICE_WIDTH / 2.0,
-    },
-    buttonsContainerTextRow: {
-        // maxHeight: FONT_SIZE,
-        // alignItems: 'center',
-        // paddingRight: 20,
-        // paddingLeft: 20,
-        // minWidth: DEVICE_WIDTH,
-        // maxWidth: DEVICE_WIDTH,
-    },
+    }
 });
